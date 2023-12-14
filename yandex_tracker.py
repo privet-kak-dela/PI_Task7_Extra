262824b106c72428b41d2623bc61199816c13756
Amalia <115399488+privet-kak-dela@users.noreply.github.com>
2023-12-14 21:21:27 +0300

    Update yandex_tracker.py

--- yandex_tracker.py
+++ yandex_tracker.py
@@ -7,16 +7,16 @@ file = open("config.txt")
 token = file.readline().strip()
 org_id = file.readline().strip()
 
-# headers = {
-#     "Host": "https://api.tracker.yandex.net",
-#     "Authorization": f"OAuth {token}",
-#     "X-Cloud-Org-ID": f"{org_id}"
-# }
+ headers = {
+    "Host": "https://api.tracker.yandex.net",
+    "Authorization": f"OAuth {token}",
+    "X-Cloud-Org-ID": f"{org_id}"
+ }
 
 client = TrackerClient(token=token,
                        cloud_org_id=org_id)
 
-# queue = client.queues['TEAMCITYBUILDFA']
+queue = client.queues['TEAMCITYBUILDFA']
 issues = client.issues.get_all()
 last_issue = str(issues[0])
 try:
@@ -24,18 +24,18 @@ try:
                          type={'name': 'Bug'})
 except Exception as ex:
     print("exception:\n", ex)
-# data = {
-#     "queue": "TEAMCITYBUILDFA",
-#     "summary": f"Issue № {last_issue[len(last_issue) - 2]}",
-#     "type": "bug",
-# }
+ data = {
+     "queue": "TEAMCITYBUILDFA",
+     "summary": f"Issue № {last_issue[len(last_issue) - 2]}",
+     "type": "bug",
+ }
 
-# //print(issues)
-# print(queue)
+ //print(issues)
+ print(queue)
 
-# try:
-#     r = requests.post(headers=headers, data=data, url="https://api.tracker.yandex.net/v2/issues/")
-#     print("Успешно")
-# except Exception as ex:
-#     print("exception", ex)
-# # r = requests.post(headers=headers, data=data,url="https://api.tracker.yandex.net/v2/issues/")
+ try:
+    r = requests.post(headers=headers, data=data, url="https://api.tracker.yandex.net/v2/issues/")
+     print("Успешно")
+ except Exception as ex:
+     print("exception", ex)
+ r = requests.post(headers=headers, data=data,url="https://api.tracker.yandex.net/v2/issues/")
